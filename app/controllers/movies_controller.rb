4286f624f7b47f20d43994f712acb47d344b69fc
Henlu Starke <energizer2k@gmail.com>
2015-11-16 21:01:25 +0000

    Done as far as i wanted to get

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,11 +11,27 @@ class MoviesController < ApplicationController
   end
 
   def index
+    redirect = false
+    redirect_options = {}
+    if params.has_key?('ratings')
+      session.delete(:ratings)
+      session[:ratings] = params[:ratings]
+    else
+      redirect = true if session.has_key?(:ratings)
+      redirect_options.store(:ratings, session[:ratings]) if session.has_key?(:ratings)
+    end
+    if params.has_key?('sorting_by')
+      session.delete(:sorting_by)
+      session[:sorting_by] = params[:sorting_by] 
+    end
+    flash.keep if redirect
+    redirect_to movies_path(redirect_options) if redirect
+
     movie_ratings = Movie.get_ratings 
-    @all_ratings, selection = ApplicationHelper.get_selection(movie_ratings, params)
-    if params.has_key?(:sorting_by)
-      @css_class = ApplicationHelper.get_index_th_css_class(params)
-      @movies = Movie.where(rating: selection).order(params[:sorting_by])
+    @all_ratings, selection = ApplicationHelper.get_selection(movie_ratings, session)
+    if session.has_key?(:sorting_by)
+      @css_class = ApplicationHelper.get_index_th_css_class(session)
+      @movies = Movie.where(rating: selection).order(session[:sorting_by])
     else
       @css_class = ApplicationHelper.get_index_th_css_class({})
       @movies = Movie.where(rating: selection)
